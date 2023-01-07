87e2c9f40d43635cb10b38c8e44588041075f744
entrydenied <benjamintak17@gmail.com>
2023-01-06 16:23:47 -0800

    Comment out old js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const squares = document.querySelectorAll('.square');
-let currentPlayer = 'X';
 
-for (let i = 0; i < squares.length; i++) {
-  squares[i].addEventListener('click', function(event) {
-    event.target.textContent = currentPlayer;
 
-    if (currentPlayer === 'X') {
-      currentPlayer = 'O';
-    } else {
-      currentPlayer = 'X';
-    }
-  });
-}
+
+
+// const squares = document.querySelectorAll('.square');
+// let currentPlayer = 'X';
+
+// for (let i = 0; i < squares.length; i++) {
+//   squares[i].addEventListener('click', function(event) {
+//     event.target.textContent = currentPlayer;
+
+//     if (currentPlayer === 'X') {
+//       currentPlayer = 'O';
+//     } else {
+//       currentPlayer = 'X';
+//     }
+//   });
+// }
